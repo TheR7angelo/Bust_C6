f46fc27570603732da1523789cebc542207d851f
TheR7angelo <raphael.antunes@hotmail.fr>
2022-11-16 13:19:10 +0100

    update

--- Bust C6/Views/MainView.xaml.cs
+++ Bust C6/Views/MainView.xaml.cs
@@ -1,9 +1,26 @@
-﻿namespace Bust_C6.Views;
+﻿using System.Windows;
+
+namespace Bust_C6.Views;
 
 public partial class MainView
 {
     public MainView()
     {
         InitializeComponent();
+    }
+    
+    private void Grid_Drop(object sender, DragEventArgs e) {
+        if (null != e.Data && e.Data.GetDataPresent(DataFormats.FileDrop)) {
+            var data = e.Data.GetData(DataFormats.FileDrop) as string[];
+            // handle the files here!
+        }
+    }
+ 
+    private void Grid_DragOver(object sender, DragEventArgs e) {
+        if (e.Data.GetDataPresent(DataFormats.FileDrop)) {
+            e.Effects = DragDropEffects.Copy;
+        } else {
+            e.Effects = DragDropEffects.None;
+        }
     }
 }
