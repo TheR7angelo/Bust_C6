78dfcd5c46909630f9a128501e3a9b6077fabd17
TheR7angelo <raphael.antunes@hotmail.fr>
2022-12-07 15:50:55 +0100

    refractor

---
+++ Libs/Sqlite/Sqlite.cs
@@ -0,0 +1,34 @@
+﻿using System.Data.SQLite;
+
+namespace Libs.Sqlite;
+
+public class Sqlite
+{
+    private SQLiteConnection Connection { get; }
+    
+    public Sqlite()
+    {
+        Connection = new SQLiteConnection(".\\Sqlite\\data.sqlite");
+    }
+
+    public string? GetCityNameByInsee(int insee)
+    {
+        var result = string.Empty;
+        var cmd = $"SELECT \"Commune\" FROM t_insee_postal WHERE \"Code INSEE\"={insee}";
+        
+        var reader = ExecuteReader(cmd);
+        while (reader.Read())
+        {
+            result = reader["Commune"].ToString();
+        }
+        reader.Close();
+
+        return result;
+    }
+
+    private SQLiteDataReader ExecuteReader(string cmd)
+    {
+        var command = new SQLiteCommand(cmd, Connection);
+        return command.ExecuteReader();
+    }
+}
